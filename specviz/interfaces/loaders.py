7f3dfb5120b02e8a7c70c74b31a6f43c03fa1264
busko <busko@stsci.edu>
2018-04-17 09:05:03 -0400

    refactor

--- specviz/interfaces/loaders.py
+++ specviz/interfaces/loaders.py
@@ -26,10 +26,13 @@ def load_yaml_reader(f_path):
 
     if any(ext in custom_loader.extension for ext in ['fits']):
         loader = FitsYamlRegister(custom_loader)
+
     elif any(ext in custom_loader.extension for ext in ['list']):
         loader = LineListYamlRegister(custom_loader)
+
     elif any(ext in custom_loader.extension for ext in ['ecsv']):
         loader = EcsvYamlRegister(custom_loader)
+
     elif any(ext in custom_loader.extension for ext in ['txt', 'dat']):
         loader = AsciiYamlRegister(custom_loader)
 
