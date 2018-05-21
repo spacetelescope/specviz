d270d8305e54f8febcbef3ecb6d0571c58a67e2e
nmearl <contact@nicholasearl.me>
2018-05-21 09:46:51 -0400

    Clear repo

--- specviz/interfaces/__init__.py
+++
@@ -1,3 +0,0 @@
-from .decorators import *
-### Don't import registires - causes an error
-#from .registries import *
--- specviz/io/loaders/__init__.py
+++
@@ -1,6 +0,0 @@
-from .apogee_loader import *
-from .generic_loader import *
-from .hstcos_loader import *
-from .hststis_loader import *
-from .sdss_loader import *
-from .ecsv_loader import *
