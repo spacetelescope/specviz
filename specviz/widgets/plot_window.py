48c99c714bcf8c68304a561dd16948387598b639
Jesse Averbukh <javerbukh@stsci.edu>
2018-05-29 14:31:53 -0400

    Custom unit validation is complete

--- specviz/widgets/plot_window.py
+++ specviz/widgets/plot_window.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import numpy as np
 import pyqtgraph as pg
 import qtawesome as qta
@@ -8,6 +9,10 @@ from qtpy.QtWidgets import QMainWindow, QMdiSubWindow, QListWidget, QAction, QDi
 from qtpy.uic import loadUi
 
 from ..utils import UI_PATH
+
+logging.basicConfig(level=logging.DEBUG, format="%(filename)s: %(levelname)8s %(message)s")
+log = logging.getLogger('UnitChangeDialog')
+log.setLevel(logging.WARNING)
 
 
 class UiPlotWindow(QMdiSubWindow):
@@ -98,11 +103,14 @@ class UnitChangeDialog(QDialog):
         """Setup the PyQt UI for this dialog."""
         self.ui.comboBox_units.addItems(self._units_titles)
         self.ui.line_custom.hide()
+        self.ui.label_valid_units.hide()
         self.ui.label_convert.setText("Convert Units from {} to: ".format(self.current_units))
 
     def setup_connections(self):
         """Setup signal/slot connections for this dialog."""
         self.ui.comboBox_units.currentTextChanged.connect(self.on_combobox_change)
+        self.ui.line_custom.textChanged.connect(self.on_line_custom_change)
+
         self.ui.buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.on_accepted)
         self.ui.buttonBox.button(QDialogButtonBox.Cancel).clicked.connect(self.on_canceled)
 
@@ -110,12 +118,46 @@ class UnitChangeDialog(QDialog):
         """Called when the text of the unit combo box has changed."""
         if self.ui.comboBox_units.currentText() == "Custom":
             self.ui.line_custom.show()
+
+            self.ui.label_valid_units.show()
+            self.ui.label_valid_units.setText("Enter custom units")
+            self.ui.label_valid_units.setStyleSheet('color: green')
         else:
             self.ui.line_custom.hide()
+            self.ui.label_valid_units.hide()
+
+    def on_line_custom_change(self):
+        """Called when the text of the custom units textbox has changed."""
+        if self.ui.line_custom.text() in ["", " "]:
+            self.ui.label_valid_units.setText("Enter custom units")
+            self.ui.label_valid_units.setStyleSheet('color: green')
+            return
+
+        try:
+            u.Unit(self.ui.line_custom.text())
+            self.ui.label_valid_units.setStyleSheet('color: green')
+            self.ui.label_valid_units.setText("{} is Valid".format(self.ui.line_custom.text()))
+
+        except Exception as e:
+            log.debug(e)
+            err = str(e)
+            if "Did you mean " in err:
+                similar_valid_units = err.split("Did you mean ")[1][:-1]
+                self.ui.label_valid_units.setText("Invalid, try: {}".format(similar_valid_units))
+            else:
+                self.ui.label_valid_units.setText("Invalid")
+
+            self.ui.label_valid_units.setStyleSheet('color: red')
 
     def on_accepted(self):
         """Called when the user clicks the "Ok" button of the dialog."""
         if self.ui.comboBox_units.currentText() == "Custom":
+            try:
+                u.Unit(self.ui.line_custom.text())
+            except Exception as e:
+                log.warning("DID NOT CHANGE UNITS. {}".format(e))
+                self.close()
+
             self.current_units = self.line_custom.text()
         else:
             self.current_units = self.ui.comboBox_units.currentText()
