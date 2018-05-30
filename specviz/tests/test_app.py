97f567baa825565f24c0511be6ceace239cf9390
nmearl <contact@nicholasearl.me>
2018-05-30 11:43:57 -0400

    Test tweaks

--- specviz/tests/test_app.py
+++ specviz/tests/test_app.py
@@ -1,13 +1,12 @@
 from qtpy.QtCore import Qt
 
 
-def test_start_main_window(qtbot):
+def test_start_main_window():
     """
     Test the ability to start the application.
     """
     from specviz.widgets.main_window import UiMainWindow
 
     window = UiMainWindow()
-    window.show()
 
     assert "Untitled Workspace" in window.windowTitle()
