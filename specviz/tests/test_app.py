7636c514279bfbe385a5fffa370064fa0245fee0
nmearl <contact@nicholasearl.me>
2018-05-30 12:02:48 -0400

    Re-introduce qtbot to basic test

--- specviz/tests/test_app.py
+++ specviz/tests/test_app.py
@@ -1,12 +1,15 @@
 from qtpy.QtCore import Qt
 
+from specviz.widgets.main_window import UiMainWindow
 
-def test_start_main_window():
+
+def test_start_main_window(qtbot):
     """
     Test the ability to start the application.
     """
-    from specviz.widgets.main_window import UiMainWindow
-
     window = UiMainWindow()
+    window.show()
+
+    qtbot.addWidget(window)
 
     assert "Untitled Workspace" in window.windowTitle()
