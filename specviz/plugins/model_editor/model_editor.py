057cba7efd57c6479ef955caee6cb5ffbeeb9176
Nicholas Earl <contact@nicholasearl.me>
2018-09-15 16:02:07 -0400

    In-prog chances to model editor

--- specviz/plugins/model_editor/model_editor.py
+++ specviz/plugins/model_editor/model_editor.py
@@ -17,23 +17,18 @@ class ModelEditor(QWidget, Plugin):
             os.path.join(os.path.dirname(__file__),
                          ".", "model_editor.ui")), self)
 
-        self.add_model_button.setIcon(qta.icon('fa.plus'))
-        self.remove_model_button.setIcon(qta.icon('fa.minus'))
-
         # Model editing
-        from ...core.models import ModelFittingModel, ModelFittingProxyModel
+        from .models import ModelFittingModel, ModelFittingProxyModel
 
         model_fitting_model = ModelFittingModel()
-        model_fitting_proxy_model = ModelFittingProxyModel()
-        model_fitting_proxy_model.setSourceModel(model_fitting_model)
+        # model_fitting_proxy_model = ModelFittingProxyModel()
+        # model_fitting_proxy_model.setSourceModel(model_fitting_model)
 
-        self.model_tree_view.setModel(model_fitting_proxy_model)
-        self.model_tree_view.setHeaderHidden(True)
-        self.parameter_tree_view.setModel(model_fitting_model)
+        self.model_tree_view.setModel(model_fitting_model)
 
-        def _set_root(idx):
-            src_idx = model_fitting_proxy_model.mapToSource(idx)
-            idx = src_idx.siblingAtColumn(1)
-            self.parameter_tree_view.setRootIndex(idx)
+        # def _set_root(idx):
+        #     src_idx = model_fitting_proxy_model.mapToSource(idx)
+        #     idx = src_idx.siblingAtColumn(1)
+        #     self.parameter_tree_view.setRootIndex(idx)
 
-        self.model_tree_view.selectionModel().currentChanged.connect(_set_root)
+        # self.model_tree_view.selectionModel().currentChanged.connect(_set_root)
