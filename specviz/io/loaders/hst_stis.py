f6df4f86b6ae6640e2b8cc1fb6e3f5d9fc8d8dde
nmearl <contact@nicholasearl.me>
2018-10-18 11:10:18 -0400

    Convert old specviz loaders into specutils loaders

--- specviz/io/loaders/hst_stis.py
+++ specviz/io/loaders/hst_stis.py
@@ -7,22 +7,24 @@ from astropy.nddata import StdDevUncertainty
 from specutils.io.registers import data_loader
 from specutils import Spectrum1D
 
-__all__ = ['cos_identify', 'cos_spectrum_loader']
+__all__ = ['stis_identify', 'stis_spectrum_loader']
 
 
-def cos_identify(*args, **kwargs):
-    """Check whether given file contains HST/COS spectral data."""
+def stis_identify(*args, **kwargs):
+    """
+    Check whether given file contains HST/STIS spectral data.
+    """
+
     with fits.open(args[0]) as hdu:
-        if hdu[0].header['TELESCOP'] == 'HST' and hdu[0].header['INSTRUME'] == 'COS':
+        if hdu[0].header['TELESCOP'] == 'HST' and hdu[0].header['INSTRUME'] == 'STIS':
            return True
 
     return False
 
 
-@data_loader(label="HST/COS", identifier=cos_identify)
-def cos_spectrum_loader(file_name, **kwargs):
-    """
-    Load file from COS spectral data into a spectrum object.
+@data_loader(label="HST/STIS",identifier=stis_identify)
+def stis_spectrum_loader(file_name, **kwargs):
+    """ Load file from STIS spectral data into a spectrum object
 
     Parameters
     ----------
@@ -43,11 +45,11 @@ def cos_spectrum_loader(file_name, **kwargs):
 
         unit = Unit("erg/cm**2 Angstrom s")
         disp_unit = Unit('Angstrom')
-        data = hdu[1].data['FLUX'].flatten() * unit
         dispersion = hdu[1].data['wavelength'].flatten() * disp_unit
+        data = hdu[1].data['FLUX'].flatten() * unit
         uncertainty = StdDevUncertainty(hdu[1].data["ERROR"].flatten() * unit)
 
-    return Spectrum1D(flux=data,
+    return Spectrum1D(flux=data * unit,
                       spectral_axis=dispersion,
                       uncertainty=uncertainty,
                       meta=meta)
