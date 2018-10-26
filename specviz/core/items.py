ce105be818ef8c6774feb74cc3272b6bebe1ec66
Jesse Averbukh <javerbukh@stsci.edu>
2018-10-26 17:45:41 -0400

    fixed uncertainty

--- specviz/core/items.py
+++ specviz/core/items.py
@@ -149,7 +149,7 @@ class PlotDataItem(pg.PlotDataItem):
         return (self.data_item.flux.unit == "" or
                 unit is not None and
                 self.data_item.flux.unit.is_equivalent(
-                    unit, equivalencies=spectral_density(self.spectral_axis)))
+                    unit, equivalencies=spectral_density(self.spectral_axis*u.Unit(self.spectral_axis_unit))))
 
     def is_spectral_axis_unit_compatible(self, unit):
         return (self.data_item.spectral_axis.unit == "" or
@@ -198,7 +198,7 @@ class PlotDataItem(pg.PlotDataItem):
 
         return uncertainty.to(self.data_unit or "",
                               equivalencies=spectral_density(
-                                  self.spectral_axis)).value
+                                  self.spectral_axis*u.Unit(self.spectral_axis_unit))).value
 
     @property
     def color(self):
