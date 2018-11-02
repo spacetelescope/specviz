c096df2b27b81b2af7ecbcca8e4fd8c5029edd10
Mees Fix <mfix@stsci.edu>
2018-11-02 11:13:19 -0400

    Correctly stores arithmetic output in data collection.

--- specviz/plugins/arithmetic/arithmetic_editor.py
+++ specviz/plugins/arithmetic/arithmetic_editor.py
@@ -39,25 +39,13 @@ class EquationEditor(QDialog):
         
     @plugin.tool_bar(name="Arithmetic", icon=QIcon(":/icons/014-calculator.svg"))
     def on_action_triggerd(self):
-        """Trigger the arithmetic UI when button is clicked.
-        """
+        """Trigger the arithmetic UI when button is clicked."""
         self.model_items = self.hub.data_items
         self.show()
 
     def set_equation(self, eq_name=None, eq_expression=None):
-        """Place equation in main dialog.
-
-        Parameters
-        ----------
-        eq_name: str
-            Name of the expression
-        eq_expression: str
-            Text of raw expression
-        """
+        """Place equation in main dialog."""
        
-        # If there is a current item selected in the tree,
-        # then see if the name of equation being passed from the
-        # editor matches the new of the selected item and edit.
         current_item = self.list_derived_components.currentItem()
         if current_item and eq_name == current_item.text(0):
             current_item.setText(1, eq_expression)
@@ -69,27 +57,22 @@ class EquationEditor(QDialog):
             self.list_derived_components.addTopLevelItem(new_row)
     
     def edit_expression(self):
-        """Edit selected expression in main dialog
-        """
+        """Edit selected expression in main dialog"""
         
         if not self.list_derived_components.currentItem():
             QMessageBox.warning(self, "No Spectrum1D Objects", 
-                                "There is no specified expression to edit!")
+                                "No selected expression to edit!")
         else:
             current_item = self.list_derived_components.currentItem()
             label = current_item.text(0)
             equation = current_item.text(1)
-
-            self.remove_arithmetic(current_item)
-
             self.editor = Editor(self, label=label, equation=equation, parent=self)
 
-    def remove_expression(self):
-        """Remove selected expression in main diaglog
-        """
+    def remove_expression(self):    
+        """Remove selected expression in main diaglog"""
         if not self.list_derived_components.currentItem():
             QMessageBox.warning(self, "No Spectrum1D Objects", 
-                                "There is no specified expression to remove!")
+                                "No selected expression to remove!")
         else:
             # Get current selected item and it's index in the QTreeWidget.
             current_item = self.list_derived_components.currentItem()
@@ -98,32 +81,15 @@ class EquationEditor(QDialog):
             self.list_derived_components.takeTopLevelItem(index)
 
     def showDialog(self):  
-        """Show arithmetic editor
-        """ 
+        """Show arithmetic editor""" 
         self.editor = Editor(self, parent=self)     
 
     def find_matches(self, eq_name):
-        """Checks for matching names
-        """
+        """Checks for matching names"""
         matches = self.list_derived_components.findItems(eq_name, Qt.MatchExactly, 0)
         matches = [item.text(0) for item in matches]
         return matches
     
-    def remove_arithmetic(self, current_item):
-        """Remove arithmetic line
-        """
-        # Get the name of the arithmetic.
-        data_name = current_item.text(0)
-        
-        # Get the names from the data items.
-        data = self.hub.workspace.model.items
-        model_names = [item.name for item in data]
-
-        # Remove data by matching location in data items by name.
-        # Hacky... if a user changes the name via the data collection list,  
-        # then I don't think this will work.
-        self.hub.workspace.model.remove_data(identifier=data[model_names.index(data_name)].identifier)
-
 class Editor(QDialog):
     tip_text = ("<b>Note:</b> Attribute names in the expression should be surrounded "
                 "by {{ }} brackets (e.g. {{{example}}}), and you can use "
@@ -141,21 +107,22 @@ class Editor(QDialog):
                         "clicking 'Insert'.")
     
     def __init__(self, equation_editor, label=None, equation=None, parent=None):
-        """Dialog where you specify equation names and expressions.
-        """
-        
+        """Dialog where you specify equation names and expressions."""
         super(Editor, self).__init__(parent)
         loadUi(os.path.abspath(
             os.path.join(os.path.dirname(__file__),
                          ".", "equation_editor.ui")), self)
     
         self._equation_editor = equation_editor
-        
+
         if not self._equation_editor.model_items:
             self.msgbox = QMessageBox.warning(self._equation_editor, "No Spectrum1D Objects", 
                                 "There is no data loaded into your SpecViz session!")
         else:
             self.is_addmode = label is None
+
+            # example=self._equation_editor.model_items
+            # self.label.setText(self.tip_text.format(example=example[0]))
 
             if label is not None:
                 self.text_label.setText(label)
@@ -186,28 +153,26 @@ class Editor(QDialog):
             self.show()
 
     def _get_eq_name(self):
+        """Get user input name of equation"""
         return self.text_label.text().strip()
 
     def _get_raw_command(self):
+        """Return text entered into the editor"""
         return self.expression.toPlainText().strip() 
 
     def _insert_component(self):
+        """Insert data item components into editor"""
         label = self.combosel_data.currentText()
         self.expression.insertPlainText('{' + label + '}' + '.' + self.combosel_component.currentText())
     
     def _assign_components(self):
-        # Assign values for name and expression in TreeWidget
+        """Assign arithmetic components to UI"""
         self.eq_name = self._get_eq_name()
         self.eq_expression = self._get_raw_command()
         
-        # Set up equation in QTreeWidget object.
         self._equation_editor.set_equation(self.eq_name, self.eq_expression)
-            
-        # Create data item from Spectrum1D object.
-        data = DataItem("New Data", data=self.evaluated_arith, identifier=uuid.uuid4())
 
-        # Add data to data collection.
-        self._equation_editor.hub.workspace.model.add_data(spec=data, name=self.eq_name)
+        self._equation_editor.hub.workspace.model.add_data(spec=self.evaluated_arith, name=self.eq_name)
         
         self._close_dialog()
     
@@ -215,9 +180,11 @@ class Editor(QDialog):
         self.close()
 
     def _item_from_name(self, name):
+        """Get data item based on name"""
         return next((x.spectrum for x in self._equation_editor.model_items if x.name == name))
     
     def _update_status(self):
+        """Check status of entered arithmetic"""
         # If the text hasn't changed, no need to check again
         if hasattr(self, '_cache') and self._cache == (self.text_label.text(), self._get_raw_command()):
             return
@@ -245,14 +212,17 @@ class Editor(QDialog):
                 self.evaluated_arith = eval(raw_str.format(**dict_map))
                 if not isinstance(self.evaluated_arith, specutils.spectra.spectrum1d.Spectrum1D):
                     raise ValueError("Arithmetic Editor must return Spectrum1D object not {}".format(type(self.evaluated_arith)))
+            
             except SyntaxError:
                 self.label_status.setStyleSheet('color: red')
                 self.label_status.setText("Incomplete or invalid syntax")
                 self.button_ok.setEnabled(False)
+            
             except Exception as exc:
                 self.label_status.setStyleSheet('color: red')
                 self.label_status.setText(str(exc))
                 self.button_ok.setEnabled(False)
+            
             else:
                 self.label_status.setStyleSheet('color: green')
                 self.label_status.setText("Valid expression")
